fc71ad4cbdb3570a6c691e6e30ca407f2f8816fa
shohrux <shohruxergashev67@gmail.com>
2022-07-19 10:14:00 +0500

    for

--- bin/for_loop07.dart
+++ bin/for_loop07.dart
@@ -1,4 +1,15 @@
 int func(List<int> numbers, int k, int n) {
+  int a = 0;
+  //int y = 0;
+
+  numbers.getRange(k, n);
+  //int a = numbers.length  1;
+  while (k < n) {
+    if (numbers[k] % 2 == 0) {
+      a += numbers[k];
+    }
+    k++;
+  }
 /*
    Given the list of numbers, return the sum of the even numbers between k and n in the list
 Args:
@@ -11,4 +22,7 @@ Return:
   return 0;
 }
 
-void main() {}
+void main() {
+  print(func([4, 2, 37, 7, 23, 2, 2], 2, 6));
+}
+
