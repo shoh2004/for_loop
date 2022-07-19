584ecd9844e60db16617646be6fb7b5eb0de0ff9
shohrux <shohruxergashev67@gmail.com>
2022-07-19 09:45:14 +0500

    for

--- bin/for_loop02.dart
+++ bin/for_loop02.dart
@@ -1,4 +1,13 @@
-int func(List<int> numbers) {
+
+  num func(List<int> numbers) {
+  int a = 0;
+  int b = numbers.length;
+  num y = 0;
+  while(b>a){
+    y += numbers[a];
+    a+=2;
+  } 
+  return y;
 /*
    Given a list of numbers, return the sum of the numbers at even index in the list
 Args:
@@ -9,4 +18,6 @@ Return:
   return 0;
 }
 
-void main() {}
+void main() {
+  print(func([3,4,2,5,7]));
+} 
