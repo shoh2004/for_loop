d754e8b4824b57fa8a79c9bd6430139de5b6ef28
shohrux <shohruxergashev67@gmail.com>
2022-07-19 09:38:35 +0500

    3280

--- bin/for_loop01.dart
+++ bin/for_loop01.dart
@@ -1,4 +1,13 @@
-int func(List numbers) {
+
+  num func(List numbers) {
+  int a = 0;
+  int b = numbers.length;
+  num d = 0;
+  while(b>a){
+    d += numbers[a];
+    a++;
+  } 
+  return d;
   /*
 Given the list of numbers, return sum of the numbers in the list
 Args:
@@ -8,5 +17,7 @@ Return:
 */
   return 0;
 }
+void main() {
+  print(func([1,2,3,4,5]));
+}
 
-void main() {}
