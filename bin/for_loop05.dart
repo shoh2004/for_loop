d4b6eac5e2960d56ddb80ed7f278f1315e274235
shohrux <shohruxergashev67@gmail.com>
2022-07-19 10:04:20 +0500

    for

--- bin/for_loop05.dart
+++ bin/for_loop05.dart
@@ -1,4 +1,13 @@
 int func(List<int> numbers) {
+  int b = 0;
+  int a = 0;
+  int y = numbers.length - 1;
+  while (b <= y) {
+    if (numbers[b] % 2 == 1) {
+      a += numbers[b];
+    }
+    b++;
+  }
 /*
    Given the list of numbers, return sum the odd numbers in the list
 Args:
@@ -9,4 +18,10 @@ Return:
   return 0;
 }
 
-void main() {}
+
+
+
+void main() {
+  print([14, 34, 34, 3, 3]);
+}
+
